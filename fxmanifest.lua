809d3305cc1784d9f49094841fa3217da740ef7d
João Tadiello - Muamba <66101564+joaotadiello@users.noreply.github.com>
2023-10-06 17:39:08 -0300

    init

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version "adamant"
+game 'gta5'
+
+ui_page 'web/build/index.html'
+
+client_scripts {
+	"client.lua"
+} 
+
+server_scripts {
+	"server.lua"
+}
+
+files {
+	'web/build/index.html',
+	'web/build/**/*',
+}
+                            
